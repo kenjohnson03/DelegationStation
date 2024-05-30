4fbfe8d1aced84d20d8dda948c0e4575805b0685
Margaret Peck <margaretpeck@microsoft.com>
2024-05-30 14:26:47 -0500

    Adding missing Model class.

---
+++ DelegationStation/Models/BulkDeviceDownloadEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DelegationStation.Models
+{
+  public class BulkDeviceDownloadEntry
+  {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string SerialNumber { get; set; }
+    public string Action { get; set; }
+    public string? AddedBy { get; set; }
+  }
+}
