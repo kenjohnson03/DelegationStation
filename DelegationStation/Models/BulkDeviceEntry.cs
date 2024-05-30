5a4d199350e218a9e69e734c83520c110a1c325c
Margaret Peck <margaretpeck@microsoft.com>
2024-05-29 19:05:41 -0500

    Replacing custom parsing with CsvHelper library.

---
+++ DelegationStation/Models/BulkDeviceEntry.cs
@@ -0,0 +1,11 @@
+﻿namespace DelegationStation.Models
+{
+  public class BulkDeviceEntry
+  {
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public string SerialNumber { get; set; }
+    public string Action { get; set; }
+
+  }
+}
