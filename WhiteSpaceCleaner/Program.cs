90c7b7ba29d45c16e6cca83c82b38e47e89d2a4e
Margaret Peck <margaretpeck@microsoft.com>
2024-06-06 19:22:34 -0500

    Migrating WhiteSpaceCleaner from function app to webjob.

---
+++ WhiteSpaceCleaner/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using DelegationStation.WhiteSpaceCleaner;
+using Microsoft.Extensions.Logging;
+
+public class Program
+{
+
+  static void Main(string[] args)
+  {
+
+    
+    using var loggerFactory = LoggerFactory.Create(builder =>
+    {
+      builder.AddConsole();
+    });
+    ILogger<WhiteSpaceCleaner> logger = loggerFactory.CreateLogger<WhiteSpaceCleaner>();
+
+    var cleaner = new WhiteSpaceCleaner(loggerFactory);
+    cleaner.Run();
+
+  }
+
+}
