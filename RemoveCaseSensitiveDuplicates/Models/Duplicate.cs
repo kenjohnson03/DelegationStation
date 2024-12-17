354e7c5c4996d4dc2252b165c82289456f8e1a43
Margaret Peck <margaretpeck@microsoft.com>
2024-12-17 17:09:28 -0600

    Adding job to cleanup entries that are now duplicates since we are doing case-insensitive matching.  Will also log any host duplicates across tags in case those need to be investigated.

---
+++ RemoveCaseSensitiveDuplicates/Models/Duplicate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveCaseSensitiveDuplicates.Models
+{
+    public class Duplicate
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+        public string Tag0 { get; set; }
+        public int Count { get; set; }
+
+        public Duplicate()
+        {
+            Make = "";
+            Model = "";
+            SerialNumber = "";
+            Tag0 = "";
+            Count = 0;
+        }
+        public Duplicate (string make, string model, string serialNumber, string tag, int count)
+        {
+            Make = make;
+            Model = model;
+            SerialNumber = serialNumber;
+            Tag0 = tag;
+            Count = count;
+        }
+
+
+    }
+}
