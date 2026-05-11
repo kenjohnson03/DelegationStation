bb90c37465648cb0dfc75333072369ab7f02cafe
Aaron Taube <aarontaube@microsoft.com>
2026-05-11 15:52:45 -0500

    Fixed error string initialization. Updated blank role name error to use error colored warning box.

--- DelegationStation/Pages/RoleEdit.razor.cs
+++ DelegationStation/Pages/RoleEdit.razor.cs
@@ -69,10 +69,11 @@ namespace DelegationStation.Pages
 
             try
             {
+                var message = "";
                 role.Name = role.Name.Trim();
                 if (string.IsNullOrWhiteSpace(role.Name))
                 {
-                    var message = $"Correlation Id: {g.ToString()}\nRole is required.";
+                    message = $"Correlation Id: {g.ToString()}\nError: Role is required.";
                     logger.LogWarning($"{message}\nUser: {userName} {userId}");
                     userMessage = message;
                     return;
@@ -87,7 +88,7 @@ namespace DelegationStation.Pages
 
                 role.Attributes.Where(a => a == AllowedAttributes.All).ToList().ForEach(a => role.Attributes.Remove(a));
                 role = await roleDBService.AddOrUpdateRoleAsync(role);
-                var message = $"Correlation Id: {g.ToString()}\nSaved role.";
+                message = $"Correlation Id: {g.ToString()}\nSaved role.";
                 logger.LogInformation($"{message}\nUser: {userName} {userId}");
                 nav.NavigateTo("/Roles");
             }
