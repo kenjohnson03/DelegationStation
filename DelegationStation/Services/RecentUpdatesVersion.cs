fad6f382f0a8c2875e13a272b438a1655fa70e06
Aaron Taube <aarontaube@microsoft.com>
2026-06-08 08:55:16 -0500

    Addressing copilot on duplicating instantiation of RecentUpdatesVersionKey.

--- DelegationStation/Services/RecentUpdatesVersion.cs
+++ DelegationStation/Services/RecentUpdatesVersion.cs
@@ -6,5 +6,6 @@ namespace DelegationStation.Services
         /// Increment this value whenever new updates are added to the RecentUpdates page.
         /// </summary>
         public const string CurrentVersion = "2.1";
+        public const string RecentUpdatesViewedVersionKey = "RecentUpdatesViewedVersion";
     }
 }
