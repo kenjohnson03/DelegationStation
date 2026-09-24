b2c93518f1ce3857975227e0d8588d59e359dcc1
Margaret Peck <margaretpeck@microsoft.com>
2026-09-24 13:14:20 -0500

    Initial version of SystemSettings object.

---
+++ DelegationSharedLibrary/Models/SystemSettings.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace DelegationSharedLibrary.Models
+{
+    public class SystemSettings
+    {
+        [Required]
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; } = "system-settings";
+        public string PartitionKey { get; set; } = "SystemSettings";
+
+        // System Settings Values
+        int MaxCorporateIdentifiers { get; set; } = 10000;
+        int ExpireProcessedDevicesDays { get; set; } = 180;
+        int ExpireUnProcessedDevicesDays { get; set; } = 180;
+
+
+        public override string ToString()
+        {
+            string output = $"Delegation Station System Settings:\n " +
+                $"MaxCorporateIdentifiers: {MaxCorporateIdentifiers}\n " +
+                $"ExpireProcessedDevicesDays: {ExpireProcessedDevicesDays}\n " +
+                $"ExpireUnProcessedDevicesDays: {ExpireUnProcessedDevicesDays}";
+            return output;
+        }
+    }
+}
